21bed7e563202a1abf2150aa5bffb022324195dc
Olivier Amblet <olivier@amblet.net>
2010-10-28 13:41:46 +0200

    Deprecate message with the old UI module.

--- lib/guard/ui.rb
+++ lib/guard/ui.rb
@@ -1,8 +1,11 @@
 module Guard
   module UI
+    
+    # this Module is deprecated, it will be removed in version 0.3.0
+    # please use the Guard::Report::ReportCenter instead
     class << self
-      
       def info(message, options = {})
+        deprecated
         unless ENV["GUARD_ENV"] == "test"
           reset_line if options[:reset]
           puts reset_color(message) if message != ''
@@ -10,13 +13,19 @@ module Guard
       end
       
       def error(message)
+        deprecated
         puts "ERROR: #{message}"
       end
       
       def debug(message)
+        deprecated
         unless ENV["GUARD_ENV"] == "test"
           puts "DEBUG: #{message}" if ::Guard.options && ::Guard.options[:debug]
         end
+      end
+      
+      def deprecated
+        puts "DEPRECATED: please use Guard::Report::ReportCenter instead of UI module in Guard plug-ins."
       end
       
       def reset_line
