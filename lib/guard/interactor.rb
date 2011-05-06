70c15a7c94b352f26f5c5cdaf1ec1528c0191fc2
Nick Sieger <nick@nicksieger.com>
2011-05-06 12:29:24 -0500

    Extract code from signal handlers into methods
    This will allow building other mechanisms to interact with Guard, for
    example on JRuby, where signal handling tends to be unreliable.

--- lib/guard/interactor.rb
+++ lib/guard/interactor.rb
@@ -1,29 +1,41 @@
 module Guard
   module Interactor
-    
+    extend self
+
+    def run_all
+      ::Guard.run do
+        ::Guard.guards.each { |guard| ::Guard.supervised_task(guard, :run_all) }
+      end
+    end
+
+    def stop
+      UI.info "Bye bye...", :reset => true
+      ::Guard.listener.stop
+      ::Guard.guards.each { |guard| ::Guard.supervised_task(guard, :stop) }
+      abort("\n")
+    end
+
+    def reload
+      ::Guard.run do
+        ::Guard.guards.each { |guard| ::Guard.supervised_task(guard, :reload) }
+      end
+    end
+
     def self.init_signal_traps
       # Run all (Ctrl-\)
       Signal.trap('QUIT') do
-        ::Guard.run do
-          ::Guard.guards.each { |guard| ::Guard.supervised_task(guard, :run_all) }
-        end
+        run_all
       end
-      
+
       # Stop (Ctrl-C)
       Signal.trap('INT') do
-        UI.info "Bye bye...", :reset => true
-        ::Guard.listener.stop
-        ::Guard.guards.each { |guard| ::Guard.supervised_task(guard, :stop) }
-        abort("\n")
+        stop
       end
-      
+
       # Reload (Ctrl-Z)
       Signal.trap('TSTP') do
-        ::Guard.run do
-          ::Guard.guards.each { |guard| ::Guard.supervised_task(guard, :reload) }
-        end
+        reload
       end
     end
-    
   end
 end
