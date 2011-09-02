170aae72036d06436af973d556d1e15151f4fc8a
Josh Lubaway <josh.lubaway@gmail.com>
2011-09-02 08:38:57 -0700

    Updating the cli growl installation message.

--- lib/guard/notifier.rb
+++ lib/guard/notifier.rb
@@ -116,7 +116,7 @@ module Guard
       end
     rescue LoadError
       turn_off
-      UI.info "Please install growl or growl_notify gem for Mac OS X notification support and add it to your Gemfile"
+      UI.info "Please install the growl gem and the growlnotify utility for Mac OS X notification support. (`gem install growl` `brew install growlnotify`)"
     end
 
     def self.require_libnotify
