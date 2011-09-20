53802ed355955ac1a45f4df80455d0277fc0d47a
Michael Kessler <michi@netzpiraten.ch>
2011-09-20 13:58:25 +0200

    Add yardoc to listeners.

--- lib/guard/listeners/linux.rb
+++ lib/guard/listeners/linux.rb
@@ -1,6 +1,11 @@
 module Guard
+
+  # Listener implementation for Linux inotify.
+  #
   class Linux < Listener
 
+    # Initialize the Listener.
+    #
     def initialize(*)
       super
       @inotify = INotify::Notifier.new
@@ -8,44 +13,53 @@ module Guard
       @latency = 0.5
     end
 
+    # Start the listener.
+    #
     def start
       @stop = false
       super
       watch_change unless watch_change?
     end
 
+    # Stop the listener.
+    #
     def stop
       super
       @stop = true
       sleep(@latency)
     end
 
+    # Check if the listener is usable on the current OS.
+    #
+    # @return [Boolean] whether usable or not
+    #
     def self.usable?
       require 'rb-inotify'
       if !defined?(INotify::VERSION) || (defined?(Gem::Version) &&
           Gem::Version.new(INotify::VERSION.join('.')) < Gem::Version.new('0.8.5'))
-        UI.info "Please update rb-inotify (>= 0.8.5)"
+        UI.info 'Please update rb-inotify (>= 0.8.5)'
         false
       else
         true
       end
     rescue LoadError
-      UI.info "Please install rb-inotify gem for Linux inotify support"
+      UI.info 'Please install rb-inotify gem for Linux inotify support'
       false
     end
 
-    def watch_change?
-      !!@watch_change
-    end
+    private
 
-  private
-
+    # Get the listener worker.
+    #
     def worker
       @inotify
     end
 
+    # Watch the given directory for file changes.
+    #
+    # @param [String] directory the directory to watch
+    #
     def watch(directory)
-      # The event selection is based on https://github.com/guard/guard/wiki/Analysis-of-inotify-events-for-different-editors
       worker.watch(directory, :recursive, :attrib, :create, :move_self, :close_write) do |event|
         unless event.name == "" # Event on root directory
           @files << event.absolute_name
@@ -54,6 +68,16 @@ module Guard
     rescue Interrupt
     end
 
+    # Test if inotify is watching for changes.
+    #
+    # @return [Boolean] whether inotify is active or not
+    #
+    def watch_change?
+      !!@watch_change
+    end
+
+    # Watch for file system changes.
+    #
     def watch_change
       @watch_change = true
       until @stop
--- lib/guard/listeners/windows.rb
+++ lib/guard/listeners/windows.rb
@@ -1,41 +1,60 @@
 module Guard
+
+  # Listener implementation for Windows fchange.
+  #
   class Windows < Listener
 
+    # Initialize the Listener.
+    #
     def initialize(*)
       super
       @fchange = FChange::Notifier.new
     end
 
+    # Start the listener.
+    #
     def start
       super
       worker.run
     end
 
+    # Stop the listener.
+    #
     def stop
       super
       worker.stop
     end
 
+    # Check if the listener is usable on the current OS.
+    #
+    # @return [Boolean] whether usable or not
+    #
     def self.usable?
       require 'rb-fchange'
       true
     rescue LoadError
-      UI.info "Please install rb-fchange gem for Windows file events support"
+      UI.info 'Please install rb-fchange gem for Windows file events support'
       false
     end
 
-  private
+    private
 
-    def worker
-      @fchange
-    end
-
+    # Watch the given directory for file changes.
+    #
+    # @param [String] directory the directory to watch
+    #
     def watch(directory)
       worker.watch(directory, :all_events, :recursive) do |event|
         paths = [File.expand_path(event.watcher.path)]
         files = modified_files(paths, :all => true)
         @callback.call(files) unless files.empty?
       end
+    end
+
+    # Get the listener worker.
+    #
+    def worker
+      @fchange
     end
 
   end
