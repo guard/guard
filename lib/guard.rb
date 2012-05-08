f47b1c48176acf7ce507ecfe40f26967538f7f09
Maher Sallam <maher@sallam.me>
2012-05-09 00:05:33 +0200

    Expand the deprecation message of the `no_vendor` option

--- lib/guard.rb
+++ lib/guard.rb
@@ -403,9 +403,11 @@ module Guard
 
     # Deprecation message for the `no_vendor` start option
     NO_VENDOR_DEPRECATION = <<-EOS.gsub(/^\s*/, '')
-      Starting with Guard v1.1 the 'no_vendor' option is removed because listening gems are now more vendored.
+      Starting with Guard v1.1 the 'no_vendor' option is removed because the monitoring gems are now
+      a part of a new gem called Listen.
 
-      Please to overwrite Listen listening gems dependencies specify custom version directly in your Gemfile.
+      You can specify specify a custom version of any monitoring gem directly in your Gemfile
+      if you want to overwrite Listen's default monitoring gems.
     EOS
 
     # Displays a warning for each deprecated options used.
