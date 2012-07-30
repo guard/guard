eb6fdd5bbeca6a817fc5e321c07c83750fdf892e
Wouter de Vos <wouter@surecreations.com>
2012-07-30 16:30:45 +0200

    Keeping the original order of preferred notifiers.

--- lib/guard/notifier.rb
+++ lib/guard/notifier.rb
@@ -158,7 +158,7 @@ module Guard
     #
     def auto_detect_notification
       # This is a duplication of `NOTIFIER.keys`, but keeps the preferred order on Ruby 1.8.7 also
-      available = [:nsnotify, :growl_notify, :gntp, :growl, :libnotify, :notifysend, :notifu, :emacs].any? { |notifier| add_notification(notifier, { }, true) }
+      available = [:growl_notify, :gntp, :growl, :libnotify, :notifysend, :notifu, :emacs, :nsnotify].any? { |notifier| add_notification(notifier, { }, true) }
       ::Guard::UI.info('Guard could not detect any of the supported notification libraries.') unless available
     end
 
