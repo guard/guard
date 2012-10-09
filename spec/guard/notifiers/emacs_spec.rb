767a79bfaa935ad9dbf7b4f5ad9ce3f8208c32aa
Dave Strock <dave.strock@gmail.com>
2012-10-09 16:49:56 -0500

    Adds ability to override default options in emacs notifier

--- spec/guard/notifiers/emacs_spec.rb
+++ spec/guard/notifiers/emacs_spec.rb
@@ -12,13 +12,28 @@ describe Guard::Notifier::Emacs do
   after { Object.send(:remove_const, :Emacs) if defined?(::Emacs) }
 
   describe '.notify' do
-    it 'should set correct modeline color using emacsclient' do
-      subject.should_receive(:system).with do |command|
-        command.should include("emacsclient")
-        command.should include("(set-face-background 'modeline \\\"ForestGreen\\\")")
-      end
+    context 'when no color options are specified' do
+      it 'should set modeline color to the default color using emacsclient' do
+        subject.should_receive(:system).with do |command|
+          command.should include("emacsclient")
+          command.should include("(set-face-background 'modeline \\\"ForestGreen\\\")")
+        end
 
-      subject.notify('success', 'any title', 'any message', 'any image', { })
+        subject.notify('success', 'any title', 'any message', 'any image', { })
+      end
+    end
+
+    context 'when a color option is specified for "success" notifications' do
+      let(:options) { {:success => 'Orange'} }
+
+      it 'should set modeline color to the specified color using emacsclient' do
+        subject.should_receive(:system).with do |command|
+          command.should include("emacsclient")
+          command.should include("(set-face-background 'modeline \\\"Orange\\\")")
+        end
+
+        subject.notify('success', 'any title', 'any message', 'any image', options)
+      end
     end
   end
 end
