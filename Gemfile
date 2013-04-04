guard/guard

commit 613b11d8713838816937c3b80f9a0596cb2de7e5

Move guard-rspec to the development group
[ci skip]

Gemfile
@@ -1,4 +1,4 @@
-source 'https://www.rubygems.org'
+source 'https://rubygems.org'
 
 gemspec
 
@@ -9,7 +9,7 @@ gem 'listen', :github => 'guard/listen'
 # installed on Travis CI.
 #
 group :development do
-
+  gem 'guard-rspec'
   gem 'guard-ronn'
 
   gem 'yard'
@@ -42,6 +42,5 @@ end
 #
 group :test do
   gem 'rspec'
-  gem 'guard-rspec'
   gem 'coveralls', :require => false
 end

commit 4a33edab4f5845de21962a24ce92b9d4b7773d8d

include guard-rspec in test group environment since it's required by guard_spec.rb
This fixes the breakage introduced by 613b11d into Travis builds.

Gemfile
@@ -43,5 +43,6 @@ end
 #
 group :test do
   gem 'rspec'
+  gem 'guard-rspec' # this is required by guard_spec.rb
   gem 'coveralls', :require => false
 end