402cf3a9bdb585eb655e38995ed0601436ac6484
Rémy Coutable <remy@rymai.me>
2013-05-16 23:54:04 +0200

    Refactor and document Guard#guards, Guard#groups, Guard#add_guard and Guard#add_group

--- lib/guard.rb
+++ lib/guard.rb
@@ -75,8 +75,6 @@ module Guard
     # @param [Hash] scopes hash with a Guard plugin or a group scope
     #
     def reload(scopes = {})
-      scopes = convert_scopes(scopes)
-
       within_preserved_state do
         ::Guard::UI.clear(:force => true)
         ::Guard::UI.action_with_scopes('Reload', scopes)
@@ -94,8 +92,6 @@ module Guard
     # @param [Hash] scopes hash with a Guard plugin or a group scope
     #
     def run_all(scopes = {})
-      scopes = convert_scopes(scopes)
-
       within_preserved_state do
         ::Guard::UI.clear(:force => true)
         ::Guard::UI.action_with_scopes('Run', scopes)
@@ -127,45 +123,39 @@ module Guard
     #   Guard.guards(/rsp.+/)
     #
     # @example Filter Guard plugins by Hash
-    #   Guard.guards({ :name => 'rspec', :group => 'backend' })
+    #   Guard.guards(:name => 'rspec', :group => 'backend')
     #
     # @param [String, Symbol, Regexp, Hash] filter the filter to apply to the Guard plugins
-    # @return [Array<Guard>] the filtered Guard plugins
+    # @return [Plugin, Array<Plugin>] the filtered Guard plugin(s)
     #
     def guards(filter = nil)
       @guards ||= []
 
       return @guards if filter.nil?
 
-      g = case filter
-          when String, Symbol
-            @guards.find_all do |guard_plugin|
-              guard_plugin.name == filter.to_s.downcase.gsub('-', '')
-            end
-          when Regexp
-            @guards.find_all do |guard_plugin|
-              guard_plugin.name =~ filter
-            end
-          when Hash
-            @guards.find_all do |guard_plugin|
-              filter.all? do |k, v|
-                case k
-                when :name
-                  guard_plugin.name == v.to_s.downcase.gsub('-', '')
-                when :group
-                  guard_plugin.group.name == v.to_sym
-                end
-              end
-            end
-          end
+      filtered_guards = case filter
+                        when String, Symbol
+                          @guards.find_all do |guard_plugin|
+                            guard_plugin.name == filter.to_s.downcase.gsub('-', '')
+                          end
+                        when Regexp
+                          @guards.find_all do |guard_plugin|
+                            guard_plugin.name =~ filter
+                          end
+                        when Hash
+                          @guards.find_all do |guard_plugin|
+                            filter.all? do |k, v|
+                              case k
+                              when :name
+                                guard_plugin.name == v.to_s.downcase.gsub('-', '')
+                              when :group
+                                guard_plugin.group.name == v.to_sym
+                              end
+                            end
+                          end
+                        end
 
-      if g.empty?
-        nil
-      elsif g.one?
-        g[0]
-      else
-        g
-      end
+      _smart_accessor_return_value(filtered_guards)
     end
 
     # Smart accessor for retrieving a specific plugin group or several plugin groups at once.
@@ -180,36 +170,31 @@ module Guard
     #   Guard.groups(/(back|front)end/)
     #
     # @param [String, Symbol, Regexp] filter the filter to apply to the Groups
-    # @return [Array<Group>] the filtered groups
+    # @return [Group, Array<Group>] the filtered group(s)
     #
     def groups(filter = nil)
       @groups ||= default_groups
 
       return @groups if filter.nil?
 
-      g = case filter
-          when String, Symbol
-            @groups.find_all { |group| group.name == filter.to_sym }
-          when Regexp
-            @groups.find_all { |group| group.name.to_s =~ filter }
-          end
+      filtered_groups = case filter
+                        when String, Symbol
+                          @groups.find_all { |group| group.name == filter.to_sym }
+                        when Regexp
+                          @groups.find_all { |group| group.name.to_s =~ filter }
+                        end
 
-      if g.empty?
-        nil
-      elsif g.one?
-        g[0]
-      else
-        g
-      end
+      _smart_accessor_return_value(filtered_groups)
     end
 
     # Add a Guard plugin to use.
     #
     # @param [String] name the Guard name
-    # @param [Array<Watcher>] watchers the list of declared watchers
-    # @param [Array<Hash>] callbacks the list of callbacks
     # @param [Hash] options the plugin options (see the given Guard documentation)
-    # @return [Guard::Plugin] the added Guard plugin
+    # @option options [String] group the group to which the Guard plugin belongs
+    # @option options [Array<Watcher>] watchers the list of declared watchers
+    # @option options [Array<Hash>] callbacks the list of callbacks
+    # @return [Plugin] the added Guard plugin
     #
     def add_guard(name, options = {})
       guard_plugin_instance = ::Guard::PluginUtil.new(name).initialize_plugin(options)
@@ -222,8 +207,10 @@ module Guard
     #
     # @param [String] name the group name
     # @option options [Boolean] halt_on_fail if a task execution
-    #   should be halted for all Guard plugins in this group if one Guard throws `:task_has_failed`
-    # @return [Guard::Group] the group added (or retrieved from the `@groups` variable if already present)
+    #   should be halted for all Guard plugins in this group if
+    #   one Guard throws `:task_has_failed`
+    # @return [Group] the group added (or retrieved from the `@groups`
+    #   variable if already present)
     #
     def add_group(name, options = {})
       group = groups(name)
@@ -267,22 +254,23 @@ module Guard
 
     private
 
-    # Convert the old scope format to the new scope format.
     #
-    # @example Convert old scopes
-    #   convert_scopes({ :guard => :rspec, :group => :backend })
-    #   => { :plugins => [:rspec], :groups => [:backend] }
     #
-    def convert_scopes(scopes)
-      if plugin = scopes.delete(:guard)
-        scopes[:plugins] = [plugin]
-      end
 
-      if group = scopes.delete(:group)
-        scopes[:groups] = [group]
-      end
 
-      scopes
+    # Given an array, returns either:
+    #   - nil if it's empty
+    #   - the first element if there's only one element
+    #   - the whole array otherwise
+    #
+    def _smart_accessor_return_value(results)
+      if results.empty?
+        nil
+      elsif results.one?
+        results[0]
+      else
+        results
+      end
     end
 
   end
