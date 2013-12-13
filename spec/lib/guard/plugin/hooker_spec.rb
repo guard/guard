e491002f8fd4476ac9517eb940b4244aa681b8a0
Rémy Coutable <remy@rymai.me>
2013-12-13 16:46:33 +0100

    Fix specs

--- spec/lib/guard/plugin/hooker_spec.rb
+++ spec/lib/guard/plugin/hooker_spec.rb
@@ -38,41 +38,50 @@ describe Guard::Plugin::Hooker do
 
   describe '.add_callback' do
     it 'can add a single callback' do
-      expect(described_class.has_callback?(listener, dummy1, :start_begin)).to be_true
+      expect(described_class.callbacks[[dummy1, :start_begin]].include?(listener)).to be_true
     end
 
     it 'can add a run_on_modifications callback' do
-      expect(described_class.has_callback?(listener, dummy1, :run_on_modifications_begin)).to be_true
+      described_class.add_callback(listener, dummy1, :run_on_modifications_begin)
+
+      expect(described_class.callbacks[[dummy1, :run_on_modifications_begin]].include?(listener)).to be_true
     end
 
     it 'can add multiple callbacks' do
       described_class.add_callback(listener, dummy1, [:event1, :event2])
-      expect(described_class.has_callback?(listener, dummy1, :event1)).to be_true
-      expect(described_class.has_callback?(listener, dummy1, :event2)).to be_true
+
+      expect(described_class.callbacks[[dummy1, :event1]].include?(listener)).to be_true
+      expect(described_class.callbacks[[dummy1, :event2]].include?(listener)).to be_true
     end
   end
 
   describe '.notify' do
     it "sends :call to the given Guard class's callbacks" do
       expect(listener).to receive(:call).with(dummy1, :start_begin, 'args')
+
       described_class.notify(dummy1, :start_begin, 'args')
     end
 
     it "sends :call to the given Guard class's run_on_modifications callback" do
-      expect(listener).to receive(:call).with(dummy1, :run_on_modifications_begin, 'args')
-      described_class.notify(dummy1, :run_on_modifications_begin, 'args')
+      expect(listener).to receive(:call).with(dummy1, :start_begin, 'args')
+
+      described_class.notify(dummy1, :start_begin, 'args')
     end
 
     it 'runs only the given callbacks' do
       listener2 = double('listener2')
       described_class.add_callback(listener2, dummy1, :start_end)
+
       expect(listener2).to_not receive(:call).with(dummy1, :start_end)
+
       described_class.notify(dummy1, :start_begin)
     end
 
     it 'runs callbacks only for the guard given' do
       described_class.add_callback(listener, dummy2, :start_begin)
+
       expect(listener).to_not receive(:call).with(dummy2, :start_begin)
+
       described_class.notify(dummy1, :start_begin)
     end
   end
@@ -81,17 +90,20 @@ describe Guard::Plugin::Hooker do
     it 'notifies the hooks' do
       expect(described_class).to receive(:notify).with(dummy1, :run_all_begin)
       expect(described_class).to receive(:notify).with(dummy1, :run_all_end)
+
       dummy1.run_all
     end
 
     it 'passes the hooks name' do
       expect(described_class).to receive(:notify).with(dummy1, :my_hook)
+
       dummy1.start
     end
 
     it 'accepts extra arguments' do
       expect(described_class).to receive(:notify).with(dummy1, :stop_begin, 'args')
       expect(described_class).to receive(:notify).with(dummy1, :special_sauce, 'first_arg', 'second_arg')
+
       dummy1.stop
     end
   end
