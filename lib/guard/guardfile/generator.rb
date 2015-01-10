e92b1ab0834da264a0629f17195597682ac2668e
Simon Kohlmeyer <simon.kohlmeyer@gmail.com>
2015-01-10 21:58:50 +0100

    Print original errors while generating Guardfiles
    This is similar to regular plugin loading, but stack traces aren't helpful here.

--- lib/guard/guardfile/generator.rb
+++ lib/guard/guardfile/generator.rb
@@ -76,13 +76,14 @@ module Guard
 
         _ui(:info, format(INFO_TEMPLATE_ADDED, plugin_name))
 
-      rescue Errno::ENOENT
+      rescue Errno::ENOENT => error
 
         name = plugin_name.downcase
         class_name = name.gsub("-", "").capitalize
         _ui(:error, "Could not load 'guard/#{name}'"\
             " or '~/.guard/templates/#{name}'"\
             " or find class Guard::#{class_name}")
+        _ui(:error, "Error is: #{error}")
       end
 
       # Adds the templates of all installed Guard implementations to an
