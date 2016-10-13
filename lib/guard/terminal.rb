5f4ab947ee501d2a8867d123443443323cf796a5
Tim Varley <tim.varley@gmail.com>
2016-10-13 15:51:01 -0400

    Fixes 858 - guard -c does not clear window buffer on OSX

--- lib/guard/terminal.rb
+++ lib/guard/terminal.rb
@@ -4,7 +4,7 @@ module Guard
   class Terminal
     class << self
       def clear
-        cmd = Gem.win_platform? ? "cls" : "clear;"
+        cmd = Gem.win_platform? ? "cls" : "printf '\33c\e[3J';"
         exit_code, _, stderr = Shellany::Sheller.system(cmd)
         fail Errno::ENOENT, stderr unless exit_code == 0
       end
