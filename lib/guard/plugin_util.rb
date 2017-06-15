d9640baed1dcde3ce7f12d7c365b99e5d8b7b95a
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2017-06-16 11:08:05 +1200

    Improve type checking
    (proposal) If you prefer to use a module for your plugin, this will break, and it's annoying to work around.

--- lib/guard/plugin_util.rb
+++ lib/guard/plugin_util.rb
@@ -55,7 +55,7 @@ module Guard
     def initialize_plugin(options)
       klass = plugin_class
       fail "Could not load class: #{_constant_name.inspect}" unless klass
-      if klass.superclass.to_s == "Guard::Guard"
+      if klass.superclass.eql?(Guard)
         klass.new(options.delete(:watchers), options)
       else
         begin
